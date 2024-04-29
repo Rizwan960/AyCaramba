928ec7d8f44fef455c81e04c4d36371f3868fb3f
Rizwan Ali <softtechnode@gmail.com>
2024-04-29 11:32:00 +0500

    In-app Purchase half done

--- lib/Views/Pages/Profile Pages/subscription_paywall_page.dart
+++ lib/Views/Pages/Profile Pages/subscription_paywall_page.dart
@@ -1,8 +1,16 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:ay_caramba/Utils/Colors/app_colors.dart';
+import 'package:ay_caramba/Utils/Common/common_data.dart';
 import 'package:ay_caramba/Utils/Fonts/app_fonts.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
 
 class SubscriptionPayWallPage extends StatefulWidget {
   final bool showAppBar;
@@ -14,58 +22,262 @@ class SubscriptionPayWallPage extends StatefulWidget {
 }
 
 class _SubscriptionPayWallPageState extends State<SubscriptionPayWallPage> {
+  final InAppPurchase _iap = InAppPurchase.instance;
+  bool initialLoadig = false;
+  bool upgrading = false;
+  bool isClicked = false;
+
+  bool gotYearly = false;
+
+  ProductDetailsResponse yearlyProductDetail =
+      ProductDetailsResponse(productDetails: [], notFoundIDs: []);
+  late StreamSubscription<List<PurchaseDetails>> _subscription;
+  final List<PurchaseDetails> _purchases = [];
+
+  @override
+  void initState() {
+    super.initState();
+    final Stream<List<PurchaseDetails>> purchaseUpdated = _iap.purchaseStream;
+    handlePurchase();
+    _subscription = purchaseUpdated.listen((purchaseDetailsList) {
+      _listenToPurchaseUpdated(purchaseDetailsList);
+      _purchases.addAll(purchaseDetailsList);
+      //  _iap.restorePurchases();
+    }, onDone: () {
+      _subscription.cancel();
+    }, onError: (error) {
+      // handle error here.
+    });
+  }
+
+  Future<void> _listenToPurchaseUpdated(
+      List<PurchaseDetails> purchaseDetailsList) async {
+    for (var purchaseDetails in purchaseDetailsList) {
+      if (purchaseDetails.pendingCompletePurchase) {
+        await _iap.completePurchase(purchaseDetails);
+      }
+      if (purchaseDetails.status == PurchaseStatus.pending) {
+        if (mounted) {
+          CommonData.showCustomSnackbar(context, 'Please wait');
+        }
+      } else {
+        if (purchaseDetails.status == PurchaseStatus.error) {
+          if (mounted) {
+            CommonData.showCustomSnackbar(
+                context, 'Un-expected error occurred');
+          }
+          setState(() {
+            upgrading = false;
+          });
+        } else if (purchaseDetails.status == PurchaseStatus.canceled) {
+          if (mounted) {
+            CommonData.showCustomSnackbar(context, 'Subscription is canceled');
+          }
+          setState(() {
+            upgrading = false;
+          });
+          _purchases.removeWhere(
+              (element) => element.productID == purchaseDetails.productID);
+        } else if (purchaseDetails.productID == "monthly_subscription" &&
+            purchaseDetails.status == PurchaseStatus.purchased) {
+          await _verifyPurchase(purchaseDetails);
+          return;
+        }
+      }
+    }
+  }
+
+  Future<void> _verifyPurchase(PurchaseDetails purchaseDetails) async {
+    try {
+      Map<String, dynamic> data = {
+        "purchase_token":
+            purchaseDetails.verificationData.serverVerificationData,
+        "purchase_id": Platform.isAndroid
+            ? purchaseDetails.productID
+            : purchaseDetails.purchaseID,
+        "is_subscribed": purchaseDetails.productID == "monthly_subscription"
+            ? "monthly"
+            : "",
+        "channel": Platform.isAndroid ? "play_store" : "app_store"
+      };
+
+      // Dio dio = await CommonData.createDioWithAuthHeader();
+      // Response response = await dio.post(AppApi.getSubscription, data: data);
+      // if (response.statusCode == 200) {
+      //   log(response.data.toString());
+      //   if (context.mounted) {
+      //     SharedPreferences prefs = await SharedPreferences.getInstance();
+      //     await prefs.setInt("isSubscribed", 1);
+      //     CommonData.isCodeValid = 1;
+      //     Navigator.of(context).pop("yes");
+      //     CommonData.showCustomSnackbar(context, "Upgraded to pro");
+
+      //     // showUpgradeDialog(context);
+      //   }
+      // }
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.unknown ||
+          e.type == DioExceptionType.connectionError ||
+          e.type == DioExceptionType.connectionTimeout) {
+        setState(() {
+          upgrading = false;
+        });
+        if (mounted) {
+          CommonData.showCustomSnackbar(context, "No internet connection");
+        }
+      } else {
+        log(e.response!.data['message'].toString());
+        if (mounted) {
+          setState(() {
+            upgrading = false;
+          });
+          CommonData.sshowDialog('Error', e.response!.data['message'], context);
+        }
+      }
+    }
+    if (mounted) {
+      setState(() {
+        upgrading = false;
+      });
+    }
+  }
+
+  Future<void> fetchProductsForYearly() async {
+    _purchases.clear();
+    if (_isPurchased(yearlyProductDetail.productDetails.first.id)) {
+      CommonData.showCustomSnackbar(context, 'Already Purchased');
+      setState(() {
+        upgrading = false;
+      });
+    } else {
+      purchaseProduct(
+        yearlyProductDetail.productDetails.first,
+      );
+    }
+  }
+
+  Future<void> purchaseProduct(ProductDetails productDetails) async {
+    final PurchaseParam purchaseParam =
+        PurchaseParam(productDetails: productDetails);
+    final purchaseDetailss = await _iap.buyNonConsumable(
+      purchaseParam: purchaseParam,
+    );
+    if (purchaseDetailss) {
+    } else {
+      if (mounted) {
+        CommonData.showCustomSnackbar(context, "Transaction failed");
+      }
+    }
+  }
+
+  Future<void> handlePurchase() async {
+    setState(() {
+      initialLoadig = true;
+    });
+    yearlyProductDetail = await _iap.queryProductDetails({
+      'monthly_subscription',
+    });
+    log(yearlyProductDetail.productDetails.toString());
+    if (yearlyProductDetail.productDetails.isEmpty) {
+      if (mounted) {
+        CommonData.showCustomSnackbar(context, 'Error while fetching details');
+        setState(() {
+          upgrading = false;
+          initialLoadig = false;
+        });
+      }
+      return;
+    } else if (yearlyProductDetail.productDetails.isNotEmpty) {
+      setState(() {
+        gotYearly = true;
+      });
+    }
+    setState(() {
+      initialLoadig = false;
+      upgrading = false;
+    });
+  }
+
+  bool _isPurchased(String productId) {
+    return _purchases.any((purchase) =>
+        purchase.productID == productId &&
+            purchase.status == PurchaseStatus.purchased ||
+        purchase.status == PurchaseStatus.restored);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.backgroundColor,
-      appBar: AppBar(
-        backgroundColor: AppColors.backgroundColor,
-        scrolledUnderElevation: 0,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SvgPicture.asset("Assets/Svg/subscribe_to_monthly.svg"),
-              const SizedBox(height: 30),
-              Stack(
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: AppColors.backgroundColor,
+          appBar: AppBar(
+            backgroundColor: AppColors.backgroundColor,
+            scrolledUnderElevation: 0,
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
                 children: [
-                  SvgPicture.asset(
-                    "Assets/Svg/subscription_box.svg",
-                    height: 350,
-                    width: double.infinity,
+                  SvgPicture.asset("Assets/Svg/subscribe_to_monthly.svg"),
+                  const SizedBox(height: 30),
+                  Stack(
+                    children: [
+                      SvgPicture.asset(
+                        "Assets/Svg/subscription_box.svg",
+                        height: 350,
+                        width: double.infinity,
+                      ),
+                      const Positioned(
+                        top: 30,
+                        left: 0,
+                        right: 0,
+                        child: Icon(
+                          CupertinoIcons.paperplane_fill,
+                          color: AppColors.callToActionColor,
+                          size: 60,
+                        ),
+                      )
+                    ],
                   ),
-                  const Positioned(
-                    top: 30,
-                    left: 0,
-                    right: 0,
-                    child: Icon(
-                      CupertinoIcons.paperplane_fill,
-                      color: AppColors.callToActionColor,
-                      size: 60,
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.callToActionColor,
+                          minimumSize: const Size.fromHeight(50)),
+                      onPressed: () {
+                        setState(() {
+                          upgrading = true;
+                        });
+                        fetchProductsForYearly();
+                      },
+                      child: const Text(
+                        "Pay Now",
+                        style: AppFonts.normalWhite18,
+                      ),
                     ),
-                  )
+                  ),
                 ],
               ),
-              const SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.callToActionColor,
-                      minimumSize: const Size.fromHeight(50)),
-                  onPressed: () {},
-                  child: const Text(
-                    "Pay Now",
-                    style: AppFonts.normalWhite18,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
+        if (initialLoadig || upgrading)
+          Container(
+            color: Colors.black.withOpacity(0.5),
+            child: const Center(
+              child: SizedBox(
+                height: 50,
+                child: SpinKitFoldingCube(
+                  color: AppColors.callToActionColor,
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
